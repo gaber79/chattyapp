06cb63d38c906c91b6a4927f162b94f3366f5d44
gaber79 <poetdreams@hotmail.com>
2016-11-03 03:48:11 +0000

    set up websocket server

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,8 @@ class App extends Component {
   updateMessages(messagevalue) {
     const newMessage = {id:(this.state.messages.length +1), username: this.state.currentUser.name, content: messagevalue }
     
-    this.setState({messages: this.state.messages.concat(newMessage)})
+    // this.setState({messages: this.state.messages.concat(newMessage)})
+    this.socket.send(JSON.stringify(newMessage))
   }
 
   updateName(name)
@@ -33,18 +34,16 @@ class App extends Component {
     this.setState({currentUser: { name }});
   }
 
-//   componentDidMount (){
-//   console.log("componentDidMount <App />");
-//   setTimeout(() => {
-//     console.log("Simulating incoming message");
-//     // Add a new message to the list of messages in the data store
-//     const newMessage = {id: 3, username: "Michelle", content: "Hello there!"};
-//     const messages = this.state.messages.concat(newMessage)
-//     // Update the state of the app component.
-//     // Calling setState will trigger a call to render() in App and all child components.
-//     this.setState({messages: messages})
-//   }, 3000);
-// }
+  componentDidMount() {
+    console.log("componentDidMount <App />")
+    this.socket = new WebSocket ('ws://localhost:4000/')
+
+    this.socket.onopen = function(event) {
+      console.log("connected to server!");
+    }
+  }
+
+
 
   render() {
     console.log("Rendering <app />")
